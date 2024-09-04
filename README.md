8cd4631ddd1123537f54947b1aa0d7324ea752c1
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-09-04 17:46:17 +0200

    Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # pods-graph
+
 Relationship between the Kubernetes pods and their volumes
+
+## What does this application do, in more detail?
+
+### This application provides a detailed and interactive way to visualize Kubernetes resources and their relationships within a cluster. Here’s a more in-depth explanation of what the application does:
+
+1. **Kubernetes Cluster Interaction:** The application connects to your Kubernetes cluster using a specified `kubeconfig` file.
+
+2. **Resource Retrieval:** The application fetches three main types of Kubernetes resources ->
+
+  -  Pods: The smallest deployable units that can be created and managed in Kubernetes, usually running one or more containers.
+
+  -  Persistent Volume Claims (PVCs): Requests for storage by users. PVCs abstract the storage resource requirements of applications.
+
+  - Persistent Volumes (PVs): Actual storage resources in the cluster that can be used by PVCs.
+
+3. **Namespace Specification:** Users can specify which namespaces to pull resources from. If no namespace is specified, the application fetches resources from all namespaces in the cluster, providing a comprehensive view.
+
+4. **Graph Creation:**
+
+  - The application constructs a directed graph using `networkx`, a Python package for creating and manipulating complex networks.
+
+  - Nodes in the graph represent the resources (Pods, PVCs, and PVs). Each node is labeled with detailed information, including the resource’s name, status, capacity, and type.
+
+  - Edges between nodes represent relationships: for example, how Pods use PVCs, and how PVCs are bound to PVs. The edges are labeled to indicate the nature of these connections. 
+
+5. **Resource Status and Capacity Visualization:** 
+
+  - The graph provides a visual overview of each resource’s status, such as whether a Pod is running, pending, or failed, or whether a PVC is bound or unbound.
+
+  - It also shows the capacity of storage resources, giving users insights into how much storage each PVC and PV is requesting or providing.
+
+6. **Error Handling:** The application includes robust error handling to deal with issues such as failed connections, missing resources, or permission errors, and outputs helpful error messages to guide users in troubleshooting.
+
+7. **Graph Visualization:**
+
+  - The graph is rendered using `matplotlib`, a plotting library that displays the graph directly in a visual format.
+
+  - This visual representation makes it easy for users to understand complex resource dependencies and quickly identify potential issues such as unbound volumes or Pods without storage.
+
+### What are the possible use cases?
+
+1. **Troubleshooting:** Quickly identify Pods that are not running as expected or PVCs that are not correctly bound to PVs.
+
+2. **Resource Management:** Gain a clear overview of how storage resources are allocated and used across different namespaces.
+
+3. **Cluster Monitoring:** Continuously monitor the health and status of Kubernetes resources in a visual format that is easier to digest than raw command-line outputs.
+
+Overall, this application simplifies the management of Kubernetes resources by transforming abstract connections into a visual map. This allows DevOps engineers, SREs, and Kubernetes administrators to quickly grasp the state of their clusters, identify potential issues, and optimize resource utilization.
+
+## Requirements:
+
+```console
+python3 -m venv venv
+source venv/bin/activate
+pip3 install kubernetes networkx matplotlib
+#`deactivate`is the command to use to exit the venv environment.
+```
+
+## How to use the script:
+
+```console
+$ python3 graph.py -h
+usage: graph.py [-h] [-k KUBECONFIG] [-n [NAMESPACES ...]]
+
+This script generates a graph of Kubernetes resources such as Pods, Persistent Volume Claims (PVCs), and Persistent Volumes (PVs). It fetches these
+resources from specified Kubernetes namespaces and visually represents their relationships.
+
+options:
+  -h, --help            show this help message and exit
+  -k KUBECONFIG, --kubeconfig KUBECONFIG
+                        Path to the kubeconfig file (default: ~/.kube/config). This file is used to authenticate and configure access to your Kubernetes
+                        cluster.
+  -n [NAMESPACES ...], --namespaces [NAMESPACES ...]
+                        Kubernetes namespace(s) to use. If not specified, the script fetches resources from all namespaces. Example: -n default kube-
+                        system.
+
+Example usage: python graph.py -k ~/.kube/config -n default
+```
--- graph.py
+++ graph.py
@@ -6,10 +6,11 @@ from kubernetes import client, config
 from concurrent.futures import ThreadPoolExecutor
 from functools import lru_cache
 
+
 def load_kube_config(kubeconfig_path):
     """
     Load the Kubernetes configuration from the specified path.
-    
+
     Args:
     kubeconfig_path (str): Path to the kubeconfig file.
     """
@@ -19,16 +20,17 @@ def load_kube_config(kubeconfig_path):
         print(f"Failed to load kubeconfig: {e}")
         raise
 
+
 @lru_cache(maxsize=None)
 def get_pods(api_instance, namespace):
     """
     Retrieve all pods in the specified Kubernetes namespace.
     If namespace is None, retrieves pods from all namespaces.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     namespace (str): Kubernetes namespace to retrieve pods from.
-    
+
     Returns:
     list: List of pods.
     """
@@ -41,16 +43,17 @@ def get_pods(api_instance, namespace):
         print(f"Failed to get pods: {e}")
         return []
 
+
 @lru_cache(maxsize=None)
 def get_pvcs(api_instance, namespace):
     """
     Retrieve all PersistentVolumeClaims (PVCs) in the specified Kubernetes namespace.
     If namespace is None, retrieves PVCs from all namespaces.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     namespace (str): Kubernetes namespace to retrieve PVCs from.
-    
+
     Returns:
     list: List of PVCs.
     """
@@ -63,14 +66,15 @@ def get_pvcs(api_instance, namespace):
         print(f"Failed to get PVCs: {e}")
         return []
 
+
 @lru_cache(maxsize=None)
 def get_volumes(api_instance):
     """
     Retrieve all PersistentVolumes (PVs) in the Kubernetes cluster.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
-    
+
     Returns:
     list: List of PVs.
     """
@@ -80,52 +84,59 @@ def get_volumes(api_instance):
         print(f"Failed to get volumes: {e}")
         return []
 
+
 def get_pod_status(api_instance, namespace, pod_name):
     """
     Retrieve the status of a pod in the specified Kubernetes namespace.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     namespace (str): Kubernetes namespace of the pod.
     pod_name (str): Name of the pod.
-    
+
     Returns:
     str: Status of the pod.
     """
     try:
-        pod_status = api_instance.read_namespaced_pod_status(name=pod_name, namespace=namespace)
+        pod_status = api_instance.read_namespaced_pod_status(
+            name=pod_name, namespace=namespace)
         return pod_status.status.phase
     except Exception as e:
-        print(f"Failed to get status for pod {pod_name} in namespace {namespace}: {e}")
+        print(
+            f"Failed to get status for pod {pod_name} in namespace {namespace}: {e}")
         return "Unknown"
+
 
 def get_pvc_status(api_instance, namespace, pvc_name):
     """
     Retrieve the status of a PVC in the specified Kubernetes namespace.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     namespace (str): Kubernetes namespace of the PVC.
     pvc_name (str): Name of the PVC.
-    
+
     Returns:
     str: Status of the PVC.
     """
     try:
-        pvc_status = api_instance.read_namespaced_persistent_volume_claim_status(name=pvc_name, namespace=namespace)
+        pvc_status = api_instance.read_namespaced_persistent_volume_claim_status(
+            name=pvc_name, namespace=namespace)
         return pvc_status.status.phase
     except Exception as e:
-        print(f"Failed to get status for PVC {pvc_name} in namespace {namespace}: {e}")
+        print(
+            f"Failed to get status for PVC {pvc_name} in namespace {namespace}: {e}")
         return "Unknown"
+
 
 def get_volume_status(api_instance, volume_name):
     """
     Retrieve the status of a PV (volume) in the Kubernetes cluster.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     volume_name (str): Name of the PV (volume).
-    
+
     Returns:
     str: Status of the PV (volume).
     """
@@ -136,33 +147,37 @@ def get_volume_status(api_instance, volume_name):
         print(f"Failed to get status for volume {volume_name}: {e}")
         return "Unknown"
 
+
 def get_pvc_capacity(api_instance, namespace, pvc_name):
     """
     Retrieve the capacity of a PVC in the specified Kubernetes namespace.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     namespace (str): Kubernetes namespace of the PVC.
     pvc_name (str): Name of the PVC.
-    
+
     Returns:
     str: Capacity of the PVC.
     """
     try:
-        pvc = api_instance.read_namespaced_persistent_volume_claim(name=pvc_name, namespace=namespace)
+        pvc = api_instance.read_namespaced_persistent_volume_claim(
+            name=pvc_name, namespace=namespace)
         return pvc.spec.resources.requests['storage']
     except Exception as e:
-        print(f"Failed to get capacity for PVC {pvc_name} in namespace {namespace}: {e}")
+        print(
+            f"Failed to get capacity for PVC {pvc_name} in namespace {namespace}: {e}")
         return "Unknown"
+
 
 def get_volume_capacity(api_instance, volume_name):
     """
     Retrieve the capacity of a PV (volume) in the Kubernetes cluster.
-    
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
     volume_name (str): Name of the PV (volume).
-    
+
     Returns:
     str: Capacity of the PV (volume).
     """
@@ -173,16 +188,17 @@ def get_volume_capacity(api_instance, volume_name):
         print(f"Failed to get capacity for volume {volume_name}: {e}")
         return "Unknown"
 
+
 def create_resource_graph(pods, pvcs, volumes, api_instance):
     """
     Create a directed graph of Kubernetes resources.
-    
+
     Args:
     pods (list): List of pods.
     pvcs (list): List of PVCs.
     volumes (list): List of PVs.
     api_instance (client.CoreV1Api): Kubernetes API instance.
-    
+
     Returns:
     networkx.DiGraph: Directed graph of resources.
     """
@@ -191,27 +207,39 @@ def create_resource_graph(pods, pvcs, volumes, api_instance):
     # Add nodes and edges for pods
     for pod in pods:
         pod_name = pod.metadata.name
-        pod_status = get_pod_status(api_instance, pod.metadata.namespace, pod_name)
-        G.add_node(pod_name, label=f"{pod_name}\nStatus: {pod_status}")  # Use the pod name and status as the label
+        pod_status = get_pod_status(
+            api_instance, pod.metadata.namespace, pod_name)
+        # Use the pod name and status as the label
+        G.add_node(pod_name, label=f"{pod_name}\nStatus: {pod_status}")
         for volume in pod.spec.volumes:
             if volume.persistent_volume_claim:
                 pvc_name = volume.persistent_volume_claim.claim_name
-                pvc_capacity = get_pvc_capacity(api_instance, pod.metadata.namespace, pvc_name)
-                pvc_status = get_pvc_status(api_instance, pod.metadata.namespace, pvc_name)
-                G.add_node(pvc_name, label=f"{pvc_name}\nCapacity: {pvc_capacity}\nStatus: {pvc_status}\nType: PVC")  # Use the PVC name, capacity, status and type as the label
+                pvc_capacity = get_pvc_capacity(
+                    api_instance, pod.metadata.namespace, pvc_name)
+                pvc_status = get_pvc_status(
+                    api_instance, pod.metadata.namespace, pvc_name)
+                # Use the PVC name, capacity, status and type as the label
+                G.add_node(
+                    pvc_name, label=f"{pvc_name}\nCapacity: {pvc_capacity}\nStatus: {pvc_status}\nType: PVC")
                 G.add_edge(pod_name, pvc_name, label='uses')
 
     # Add nodes and edges for PVCs
     for pvc in pvcs:
         pvc_name = pvc.metadata.name
-        pvc_capacity = get_pvc_capacity(api_instance, pvc.metadata.namespace, pvc_name)
-        pvc_status = get_pvc_status(api_instance, pvc.metadata.namespace, pvc_name)
-        G.add_node(pvc_name, label=f"{pvc_name}\nCapacity: {pvc_capacity}\nStatus: {pvc_status}\nType: PVC")  # Use the PVC name, capacity, status and type as the label
+        pvc_capacity = get_pvc_capacity(
+            api_instance, pvc.metadata.namespace, pvc_name)
+        pvc_status = get_pvc_status(
+            api_instance, pvc.metadata.namespace, pvc_name)
+        # Use the PVC name, capacity, status and type as the label
+        G.add_node(
+            pvc_name, label=f"{pvc_name}\nCapacity: {pvc_capacity}\nStatus: {pvc_status}\nType: PVC")
         if pvc.spec.volume_name:
             volume_name = pvc.spec.volume_name
             volume_capacity = get_volume_capacity(api_instance, volume_name)
             volume_status = get_volume_status(api_instance, volume_name)
-            G.add_node(volume_name, label=f"{volume_name}\nCapacity: {volume_capacity}\nStatus: {volume_status}\nType: Volume")  # Use the PV name, capacity, status and type as the label
+            # Use the PV name, capacity, status and type as the label
+            G.add_node(
+                volume_name, label=f"{volume_name}\nCapacity: {volume_capacity}\nStatus: {volume_status}\nType: Volume")
             G.add_edge(pvc_name, volume_name, label='bound to')
 
     # Add nodes for Volumes
@@ -219,14 +247,17 @@ def create_resource_graph(pods, pvcs, volumes, api_instance):
         volume_name = volume.metadata.name
         volume_capacity = get_volume_capacity(api_instance, volume_name)
         volume_status = get_volume_status(api_instance, volume_name)
-        G.add_node(volume_name, label=f"{volume_name}\nCapacity: {volume_capacity}\nStatus: {volume_status}\nType: Volume")  # Use the PV name, capacity, status and type as the label
+        # Use the PV name, capacity, status and type as the label
+        G.add_node(
+            volume_name, label=f"{volume_name}\nCapacity: {volume_capacity}\nStatus: {volume_status}\nType: Volume")
 
     return G
+
 
 def draw_graph(G):
     """
     Draw the Kubernetes resource graph.
-    
+
     Args:
     G (networkx.DiGraph): Directed graph of resources.
     """
@@ -235,74 +266,82 @@ def draw_graph(G):
     node_labels = nx.get_node_attributes(G, 'label')
 
     plt.figure(figsize=(14, 10))
-    nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=3000, node_color='lightblue', font_size=10, font_weight='bold')
+    nx.draw(G, pos, with_labels=True, labels=node_labels, node_size=3000,
+            node_color='lightblue', font_size=10, font_weight='bold')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='red')
     plt.title("Kubernetes Resource Graph")
     plt.show()
 
+
 def fetch_resources(api_instance, namespaces):
     """
-    Fetch Kubernetes resources (pods, PVCs, volumes) in parallel.
-    
+    Fetch Kubernetes resources (pods, PVCs, volumes) from the specified namespaces.
+
     Args:
     api_instance (client.CoreV1Api): Kubernetes API instance.
-    namespaces (list): List of Kubernetes namespaces to fetch resources from.
-    
+    namespaces (list): List of namespaces to fetch resources from.
+
     Returns:
-    tuple: Lists of pods, PVCs, and PVs.
+    tuple: Lists of pods, PVCs, and volumes.
     """
     pods = []
     pvcs = []
-    volumes = []
 
-    with ThreadPoolExecutor() as executor:
-        pod_futures = {executor.submit(get_pods, api_instance, namespace): namespace for namespace in namespaces}
-        for future in pod_futures:
-            namespace = pod_futures[future]
-            try:
-                pods.extend(future.result())
-            except Exception as e:
-                print(f"Failed to fetch pods from namespace {namespace}: {e}")
+    if namespaces:
+        with ThreadPoolExecutor() as executor:
+            pods = sum(executor.map(lambda ns: get_pods(
+                api_instance, ns), namespaces), [])
+            pvcs = sum(executor.map(lambda ns: get_pvcs(
+                api_instance, ns), namespaces), [])
+    else:
+        pods = get_pods(api_instance, None)
+        pvcs = get_pvcs(api_instance, None)
 
-        pvc_futures = {executor.submit(get_pvcs, api_instance, namespace): namespace for namespace in namespaces}
-        for future in pvc_futures:
-            namespace = pvc_futures[future]
-            try:
-                pvcs.extend(future.result())
-            except Exception as e:
-                print(f"Failed to fetch PVCs from namespace {namespace}: {e}")
-
-        try:
-            volumes = get_volumes(api_instance)
-        except Exception as e:
-            print(f"Failed to fetch volumes: {e}")
-
+    volumes = get_volumes(api_instance)
     return pods, pvcs, volumes
+
 
 def main(kubeconfig_path, namespaces):
     """
-    Main function to load configuration, fetch resources, and create the graph.
-    
+    Main function to generate and display the Kubernetes resource graph.
+
     Args:
     kubeconfig_path (str): Path to the kubeconfig file.
-    namespaces (str or list): Kubernetes namespace(s) to use. If None or empty list, uses all namespaces.
+    namespaces (list): List of namespaces to fetch resources from.
     """
     load_kube_config(kubeconfig_path)
     api_instance = client.CoreV1Api()
 
-    if not namespaces:
-        namespaces = [None]  # If no namespaces specified, use None to indicate all namespaces
-
     pods, pvcs, volumes = fetch_resources(api_instance, namespaces)
-    resource_graph = create_resource_graph(pods, pvcs, volumes, api_instance)
-    draw_graph(resource_graph)
+    G = create_resource_graph(pods, pvcs, volumes, api_instance)
+    draw_graph(G)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a graph of Kubernetes resources")
-    parser.add_argument('-k', '--kubeconfig', type=str, default=os.path.expanduser("~/.kube/config"),
-                        help='Path to the kubeconfig file (default: ~/.kube/config)')
-    parser.add_argument('-n', '--namespaces', type=str, nargs='*', default=None,
-                        help='Kubernetes namespace(s) to use. If not specified, uses all namespaces.')
+    parser = argparse.ArgumentParser(
+        description=(
+            "This script generates a graph of Kubernetes resources such as Pods, Persistent Volume Claims (PVCs), "
+            "and Persistent Volumes (PVs). It fetches these resources from specified Kubernetes namespaces and "
+            "visually represents their relationships.\n\n"
+        ),
+        epilog=(
+            "Example usage:\n"
+            "  python graph.py -k ~/.kube/config -n default\n"
+        )
+    )
+    parser.add_argument(
+        '-k', '--kubeconfig',
+        type=str,
+        default=os.path.expanduser("~/.kube/config"),
+        help='Path to the kubeconfig file (default: ~/.kube/config). This file is used to authenticate and configure access to your Kubernetes cluster.'
+    )
+    parser.add_argument(
+        '-n', '--namespaces',
+        type=str,
+        nargs='*',
+        default=None,
+        help='Kubernetes namespace(s) to use. If not specified, the script fetches resources from all namespaces. Example: -n default kube-system.'
+    )
     args = parser.parse_args()
 
     main(args.kubeconfig, args.namespaces)
